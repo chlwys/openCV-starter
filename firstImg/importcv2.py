a6c54c14af5bcd2ac4874b57166e0407b20d7e21
chlwys <joshuaweeys@gmail.com>
2020-12-20 18:21:08 +0800

    firstimg

---
+++ firstImg/importcv2.py
@@ -0,0 +1,12 @@
+#this code opens an image with openCV
+
+#import openCV 
+import cv2
+
+#use openCV to read an image, store image as 'src'
+src = cv2.imread("tzj.jpg", cv2.IMREAD_ANYCOLOR)
+
+#display image in a window
+cv2.imshow("Image", src)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
